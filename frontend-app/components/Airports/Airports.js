54bc20bd3fdfcccd1cc3be25087a814ee924ae77
Nachiket <nmore@pdx.edu>
2023-11-09 13:30:27 -0800

    fix

---
+++ frontend-app/components/Airports/Airports.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Airports = () => {
+  return <div className="airports-container"></div>;
+};
+
+export default Airports;
---
+++ frontend-app/components/Flights/Flights.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Flights = () => {
+  return <div className="flights-container"></div>;
+};
+
+export default Flights;
