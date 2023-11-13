c5e327b11b7e31c08e9e0bdb1c646d4692b2a0ee
Nachiket <nmore@pdx.edu>
2023-11-13 12:47:40 -0800

    added routes

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Airlines from "./components/Airlines/Airlines";
+import Airports from "./components/Airports/Airports";
+import Flights from "./components/Flights/Flights";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          <Navbar />
+
+          <Routes>
+            <Route exact path="/airports" element={<Airlines />} />
+            <Route exact path="/airlines" element={<Airports />} />
+            <Route exact path="/flights" element={<Flights />} />
+            <Route exact path="/navbar" element={<Navbar />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
