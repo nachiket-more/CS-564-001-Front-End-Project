db5c9325565aa939e8615393c63d0f07d89f1b2f
SayleeMJ <sjagtap@pdx.edu>
2023-11-29 14:16:11 -0800

    Changed double quotation marks to single quotation marks

--- backend-app/src/routes/airlinesRoutes.js
+++ backend-app/src/routes/airlinesRoutes.js
@@ -1,18 +1,18 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 // Define the route to get all airlines
-router.get("/all", async (req, res) => {
+router.get('/all', async (req, res) => {
   try {
-    const airlineCollection = mongoose.connection.collection("airlines");
+    const airlineCollection = mongoose.connection.collection('airlines');
 
     const airlines = await airlineCollection.find({}).toArray();
 
     res.json(airlines);
   } catch (error) {
-    console.error("Error fetching airlines:", error);
-    res.status(500).json({ error: "Internal Server Error" });
+    console.error('Error fetching airlines:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
--- backend-app/src/routes/airportsRoutes.js
+++ backend-app/src/routes/airportsRoutes.js
@@ -1,18 +1,18 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 // Define the route to get all airports
-router.get("/all", async (req, res) => {
+router.get('/all', async (req, res) => {
   try {
-    const airportsCollection = mongoose.connection.collection("airports");
+    const airportsCollection = mongoose.connection.collection('airports');
 
     const airports = await airportsCollection.find({}).toArray();
 
     res.json(airports);
   } catch (error) {
-    console.error("Error fetching airports:", error);
-    res.status(500).json({ error: "Internal Server Error" });
+    console.error('Error fetching airports:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
