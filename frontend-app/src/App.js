b8b9156eafa7d205272226908b211f6403012394
Nachiket <nmore@pdx.edu>
2023-11-30 21:13:04 -0800

    fixed route issue

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,22 +1,24 @@
-import React from 'react';
+import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 
 import Airlines from "./components/Airlines/Airlines";
 import Airports from "./components/Airports/Airports";
 import Flights from "./components/Flights/Flights";
 import Navbar from "./components/Navbar/Navbar";
-import { DataProvider } from './context/DataContext';
+import { DataProvider } from "./context/DataContext";
 
-
-import { useDispatch } from 'react-redux';
-import { fetchAirlinesData } from './redux/actions/airlineAction';
-import { fetchFlightsData } from './redux/actions/flightsAction';
+import { useDispatch } from "react-redux";
+import { fetchAirlinesData } from "./redux/actions/airlineAction";
+import { fetchFlightsData } from "./redux/actions/flightsAction";
 
 function App() {
   const dispatch = useDispatch();
-
-
 
   React.useEffect(() => {
     // Fetch airlines and flights data when the component mounts
@@ -24,23 +26,24 @@ function App() {
     dispatch(fetchFlightsData());
   }, [dispatch]);
 
-
   return (
     <DataProvider>
-    <div className="App">
-      <Router>
-        <div>
-          <Navbar />
+      <div className="App">
+        <Router>
+          <div>
+            <Navbar />
 
-          <Routes>
-            <Route exact path="/airports" element={<Airports />} />
-            <Route exact path="/airlines" element={<Airlines />} />
-            <Route exact path="/flights" element={<Flights />} />
-            <Route exact path="/navbar" element={<Navbar />} />
-          </Routes>
-        </div>
-      </Router>
-    </div>
+            <Routes>
+              <Route path="/" element={<Navigate to="/airlines" />} />
+              <Route path="/index.js" element={<Navigate to="/airlines" />} />
+              <Route exact path="/airports" element={<Airports />} />
+              <Route exact path="/airlines" element={<Airlines />} />
+              <Route exact path="/flights" element={<Flights />} />
+              <Route exact path="/navbar" element={<Navbar />} />
+            </Routes>
+          </div>
+        </Router>
+      </div>
     </DataProvider>
   );
 }
